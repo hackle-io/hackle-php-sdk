45e12746171f73aa63e0af6e7e75897ac781a5ec
yong <yong@hackle.io>
2023-05-22 15:12:32 +0900

    Workspcae

--- tests/Internal/Workspace/ResourcesWorkspaceFetcher.php
+++ tests/Internal/Workspace/ResourcesWorkspaceFetcher.php
@@ -3,7 +3,6 @@
 namespace Internal\Workspace;
 
 use Hackle\Internal\Workspace\DefaultWorkspace;
-use Hackle\Internal\Workspace\Dto\WorkspaceDto;
 use Hackle\Internal\Workspace\Workspace;
 use Hackle\Internal\Workspace\WorkspaceFetcher;
 
@@ -15,8 +14,7 @@ class ResourcesWorkspaceFetcher implements WorkspaceFetcher
     public function __construct($resourcesPath)
     {
         $json = file_get_contents($resourcesPath);
-        $dto = WorkspaceDto::decode(json_decode($json, true));
-        $this->workspace = DefaultWorkspace::from($dto);
+        $this->workspace = DefaultWorkspace::from(json_decode($json, true));
     }
 
 
